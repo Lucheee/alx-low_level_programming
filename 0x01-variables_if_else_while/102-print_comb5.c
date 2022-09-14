6d2f78b50af27ed348ee5aad6e55264ddc4d0208
Lucheee <oluchie51@gmail.com>
2022-09-14 02:49:15 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * main - Entry point
+ * Description: a program to print all possible different combination
+ *     of two digit numbers
+ * Return: Always 0 (Success)
+ * Author: Omotoye Shamsudeen Adekoya
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a < 10; a++)
+
+
